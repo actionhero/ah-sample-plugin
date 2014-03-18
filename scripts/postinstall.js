aad9db03543c85c1f9a6486a6f204b19d4056309
Evan Tahler <evan@taskrabbit.com>
2014-03-18 16:53:14 -0700

    move to plugins

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var path = require('path');
 
 var localFile   = path.normalize(__dirname + '/../config/ah-sample-plugin.js');
-var projectFile = path.normalize(process.cwd() + '/../../config/ah-sample-plugin.js');
+var projectFile = path.normalize(process.cwd() + '/../../config/plugins/ah-sample-plugin.js');
 
 if(!fs.existsSync(projectFile)){
   console.log("coppying " + localFile + " to " + projectFile)
