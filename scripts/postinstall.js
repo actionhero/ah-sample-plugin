cddae8212dcced2a03c7795e33d107f4488a942e
Evan Tahler <evan@taskrabbit.com>
2014-03-20 11:36:03 -0700

    make the dir

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,10 +3,14 @@
 var fs = require('fs');
 var path = require('path');
 
-var localFile   = path.normalize(__dirname + '/../config/ah-sample-plugin.js');
-var projectFile = path.normalize(process.cwd() + '/../../config/plugins/ah-sample-plugin.js');
+var localFile   = path.normalize(__dirname + '/../config/airbrake.js');
+var projectPath = path.normalize(process.cwd() + '/../../config/plugins');
+var projectFile = path.normalize(process.cwd() + '/../../config/plugins/airbrake.js');
 
 if(!fs.existsSync(projectFile)){
   console.log("coppying " + localFile + " to " + projectFile)
+  try{
+    fs.mkdirSync(projectPath);
+  }catch(e){ }
   fs.createReadStream(localFile).pipe(fs.createWriteStream(projectFile));
 }
