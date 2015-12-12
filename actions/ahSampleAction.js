388aab137362fd6737a33d4cf4c7457e27b262ce
Evan Tahler <evantahler@gmail.com>
2015-12-11 20:30:23 -0800

    update for actionhero v12

--- actions/ahSampleAction.js
+++ actions/ahSampleAction.js
@@ -3,8 +3,8 @@ exports.action = {
   description: 'I say hello from middleware',
   inputs: {},
 
-  run: function(api, connection, next){
-    connection.response.message = api.config['ah-sample-plugin'].message;
+  run: function(api, data, next){
+    data.connection.response.message = api.config['ah-sample-plugin'].message;
     next(connection, true);
   }
 };
--- initializers/ah_sample_plugin.js
+++ initializers/ah_sample_plugin.js
@@ -1,9 +1,19 @@
 module.exports = {
   start: function(api, next){
-    api.actions.postProcessors.push(function(connection, actionTemplate, toRender, next){
-      connection.response._time    = String(new Date());
-      connection.response._message = api.config['ah-sample-plugin'].message;
-      next(connection, toRender);
-    });
+
+    var middleware = {
+      name: 'ah-sample-plugin-middleware',
+      global: true,
+      priority: 1000,
+      postProcessor: function(data, done){
+        data.connection.response._time = String(new Date());
+        data.connection.response._message = api.config['ah-sample-plugin'].message;
+        done();
+      }
+    };
+
+    api.actions.addMiddleware(middleware);
+
+    next();
   }
 };
